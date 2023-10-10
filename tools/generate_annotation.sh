c2a47de2ed3b3efd9349b2390cadef8da8620c0c
angusku <kjm15246@gmail.com>
2023-10-10 20:59:10 +0800

    feat: script for generating voc annotation file

---
+++ tools/generate_annotation.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+source_path=$1
+output_path=$2
+
+output_file_name='annotation.txt'
+
+if [ -z "$source_path" ] || [ -z "$output_path" ]
+    then
+    echo "no argument"
+    echo "please assign \"source_path\" and \"output_path\""
+    echo "ex: ${0} dataset/images dataset/annotation"
+    exit 1
+fi
+
+# echo "ls ${source_path} -1 | grep xml | sed -e 's/.xml//'"
+ls ${source_path} -1 | grep xml | sed -e 's/.xml//' > ${output_path}/${output_file_name}
