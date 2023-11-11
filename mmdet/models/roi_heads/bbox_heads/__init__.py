929176f4e01efacf4ac3c26b9ccd335e593bdb63
angusku <kjm15246@gmail.com>
2023-11-11 13:22:43 +0800

    fix: miss import DIIHead_FC_Attention

--- mmdet/models/roi_heads/bbox_heads/__init__.py
+++ mmdet/models/roi_heads/bbox_heads/__init__.py
@@ -3,6 +3,7 @@ from .bbox_head import BBoxHead
 from .convfc_bbox_head import (ConvFCBBoxHead, Shared2FCBBoxHead,
                                Shared4Conv1FCBBoxHead)
 from .dii_head import DIIHead
+from .dii_head_fc_attention import DIIHead_FC_Attention
 from .double_bbox_head import DoubleConvFCBBoxHead
 from .multi_instance_bbox_head import MultiInstanceBBoxHead
 from .sabl_head import SABLHead
