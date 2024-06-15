6dea06e07f00bfae6c5c8dd2e24610291bd668aa
angusku <kjm15246@gmail.com>
2024-06-15 15:15:16 +0800

    refactor: remove unused model config

--- configs/strawberry_disease/sparse-rcnn_r50_mdconv-c3-c5_fpn_4x_voc.py
+++
@@ -1,6 +0,0 @@
-_base_ = './sparse-rcnn_r50_fpn_4x_voc.py'
-
-model = dict(
-    backbone=dict(
-        dcn=dict(type='DCNv2', deform_groups=1, fallback_on_stride=False),
-        stage_with_dcn=(False, True, True, True)))
